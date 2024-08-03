754133601f88da401402a907d12710d06d4581a7
kaue_sv <kauesousavieira534@gmail.com>
2024-08-03 13:13:00 -0300

    Adicionando autenticações e permissões nas APIS

--- escola/views.py
+++ escola/views.py
@@ -1,28 +1,43 @@
 from escola.models import Estudante, Curso, Matricula
 from escola.serializers import EstudanteSerializer, CursoSerializer, MatriculaSerializer, ListaMatriculasCursoSerializer, ListaMatriculasEstudanteSerializer
 from rest_framework import viewsets, generics
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, IsAuthenticatedOrReadOnly
 
 
 class EstudanteViewSet(viewsets.ModelViewSet):
+    #authentication_classes = [BasicAuthentication]
+    #permission_classes = [IsAuthenticated]
+
     queryset = Estudante.objects.all()
     serializer_class = EstudanteSerializer
 
 class CursoViewSet(viewsets.ModelViewSet):
+    #authentication_classes = [BasicAuthentication]
+    #permission_classes = [IsAuthenticated]
+
     queryset = Curso.objects.all()
     serializer_class = CursoSerializer
 
 class MatriculaViewSet(viewsets.ModelViewSet):
+    #authentication_classes = [BasicAuthentication]
+    #permission_classes = [IsAuthenticated]
+
     queryset = Matricula.objects.all()
     serializer_class = MatriculaSerializer
 
 class ListaMatriculasEstudante(generics.ListAPIView):
+    #authentication_classes = [BasicAuthentication]
+    #permission_classes = [IsAuthenticated]
 
     serializer_class = ListaMatriculasEstudanteSerializer
     def get_queryset(self):
         return Matricula.objects.filter(estudante_id=self.kwargs['pk'])
 
 class ListaMatriculasCurso(generics.ListAPIView):
-    
+    #authentication_classes = [BasicAuthentication]
+    #permission_classes = [IsAuthenticated]
+
     serializer_class = ListaMatriculasCursoSerializer
     def get_queryset(self):
         return Matricula.objects.filter(estudante_id=self.kwargs['pk'])
--- setup/settings.py
+++ setup/settings.py
@@ -124,3 +124,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Ao inves de colocar em cada view suas permissoes e autenticações, defini aqui um padrao para todas
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+}
